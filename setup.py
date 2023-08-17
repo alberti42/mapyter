b8d964629442512cc7483a972f61d3d10004f51e
Andrea Alberti <a.alberti82@gmail.com>
2023-08-17 13:24:01 +0200

    added manifest

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'Mapyter',
-  packages = ['mapyter'],
+  packages = ['mapyter','mapyter.magics'],
   package_dir = {'':'src'},
   version = '1.0',
   description = 'Mapyter - The MATLAB interface for Jupyter',
   author = 'Andrea Alberti',
   author_email = 'a.alberti82@gmail.com',
+  url='https://github.com/alberti42/mapyter',
   install_requires=[
         "tqdm",
         "pillow",
---
+++ src/mapyter/magics/__init__.py
@@ -0,0 +1,3 @@
+"""MAGICS for Jupyter Mapyter"""
+
+__version__ = '1.0'
