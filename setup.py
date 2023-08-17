f760b762882df698be9070d387bcdaf1dae50f98
Andrea Alberti <a.alberti82@gmail.com>
2023-08-17 11:45:52 +0200

    Moved to subfolder

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'Mapyter',
+  packages = ['mapyter'],
+  version = '1.0',
+  description = 'Mapyter - The MATLAB interface for Jupyter',
+  author = 'Andrea Alberti',
+  author_email = 'a.alberti82@gmail.com',
+  install_requires=[
+        "tqdm",
+        "pillow",
+        "jupyter-packaging"
+    ],
+)
